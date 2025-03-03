68639868de8cdaf687f54c948dcdb60d09effeb0
iksurainura <158151788+iksurainura@users.noreply.github.com>
2025-03-04 02:55:38 +0530

    Add files via upload

---
+++ booking/src/main/java/com/wondersri/booking/BookingApplication.java
@@ -0,0 +1,15 @@
+package com.wondersri.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class BookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingApplication.class, args);
+	}
+
+}
