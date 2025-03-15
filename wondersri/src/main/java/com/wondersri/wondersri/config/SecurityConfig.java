83db7e6946c458b73829a25965c38726d3bd6136
iksurainura <158151788+iksurainura@users.noreply.github.com>
2025-03-15 10:55:43 +0530

    v

--- wondersri/src/main/java/com/wondersri/wondersri/config/SecurityConfig.java
+++ wondersri/src/main/java/com/wondersri/wondersri/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,10 +15,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // Allow all requests without authentication
-                )
-                .csrf(csrf -> csrf.disable()); // Disable CSRF protection (optional)
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/Dashboard").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic();
+
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
