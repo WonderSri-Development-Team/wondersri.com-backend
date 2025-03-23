4a68efc966cd2aa5540c1368d8c6c1168ab08c73
iksurainura <158151788+iksurainura@users.noreply.github.com>
2025-03-23 17:39:40 +0530

    UPDATES

--- wondersri/src/main/java/com/wondersri/wondersri/config/WebConfig.java
+++ wondersri/src/main/java/com/wondersri/wondersri/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.wondersri.config;
+package com.wondersri.wondersri.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
