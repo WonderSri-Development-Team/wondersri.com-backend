aa2262af5e62d2f91f52fca6ad740de70582992a
iksurainura <158151788+iksurainura@users.noreply.github.com>
2025-03-23 18:45:34 +0530

    updated the time slots

--- wondersri/src/main/java/com/wondersri/wondersri/Enum/TimeSlot.java
+++ wondersri/src/main/java/com/wondersri/wondersri/Enum/TimeSlot.java
@@ -1,8 +1,8 @@
 package com.wondersri.wondersri.Enum;
 
 public enum TimeSlot {
-    SLOT_1("08:00-01:00"),
-    SLOT_2("01:00-05:00");
+    SLOT_1("08:00 am -01:00 pm"),
+    SLOT_2("01:00 pm -05:00 pm");
     private final String displayName;
     TimeSlot(String displayName) {
         this.displayName = displayName;
